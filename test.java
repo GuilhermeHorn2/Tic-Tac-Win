41f4c71622ebee35d606ab1956b3762513655b18
GuilhermeHorn2 <74984552+GuilhermeHorn2@users.noreply.github.com>
2023-06-26 07:59:55 -0300

    Create test.java

---
+++ test.java
@@ -0,0 +1,21 @@
+package misc;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		tic_tac_toe_check t = new tic_tac_toe_check();
+		t.random_board();
+		t.print_board();
+		t.win(1);
+		t.win(0);
+
+		
+	
+}
+	
+	
+	
+}	
